4fe323e42df8c8f14911e2ec6884e4f8390451a1
Kimjisungs <kjsqwe9@gmail.com>
2019-11-19 21:01:03 +0900

    feat: repectoring ing

--- public/js/weather.js
+++ public/js/weather.js
@@ -1,4 +1,5 @@
 const $weater = document.querySelector('.weather');
+const $wrapper = document.querySelector('.wrapper');
 
 let weathers = {};
 
@@ -6,28 +7,55 @@ const promiseGetWeather = () => axios.get('http://api.openweathermap.org/data/2.
 
 const ajaxWeather = async () => {
   try {
-  weathers = await promiseGetWeather();
-  renderWeather();
-  } catch(e) {
+    weathers = await promiseGetWeather();
+    renderWeather();
+  } catch (e) {
     console.log(new Error('Error'));
   }
 };
 
 const renderWeather = () => {
+
+  const dataWeather = {
+    nowWeather: weathers.data.weather[0].main,
+    icon: weathers.data.weather[0].icon,
+    country: weathers.data.sys.country,
+    cityName: weathers.data.name
+  };
+
+  const {
+    nowWeather, icon, country, cityName
+  } = dataWeather;
+
+
   let html = '';
   html = `
-  <div class="image"><img src="http://openweathermap.org/img/w/${weathers.data.weather[0].icon}.png"></div>
+  <div class="image"><img src="http://openweathermap.org/img/w/${icon}.png"></div>
   <div class="description">
-    <p class="country">${weathers.data.sys.country} - <span class="country-name">${weathers.data.name}</span></p>
-    <h3 class="now-weather">${weathers.data.weather[0].main}</h3>
+    <p class="country">${country} - <span class="country-name">${cityName}</span></p>
+    <h3 class="now-weather">${nowWeather}</h3>
   </div>
-  `
+  `;
   $weater.innerHTML = html;
   // console.log('현재 날씨' + weathers.data.weather[0].main);
   // console.log('아이콘' + weathers.data.weather[0].icon);
   // console.log('상세 날씨 설명' + weathers.data.weather[0].description);
   // console.log('나라' + weathers.data.sys.country);
   // console.log('도시이름' + weathers.data.name);
+
+  weatherBackground(nowWeather);
+};
+
+const weatherBackground = (nowWeather) => {
+  const wrapperBg = $wrapper.style;
+  switch (nowWeather) {
+    case 'Clear':
+      wrapperBg.backgroundImage = 'url("https://user-images.githubusercontent.com/33679192/69144269-41d2c080-0b0e-11ea-98c2-017b4739d299.jpg")';
+      break;
+    default:
+      wrapperBg.backgroundColor = 'white';
+      break;
+  }
 };
 
 window.addEventListener('load', () => {
